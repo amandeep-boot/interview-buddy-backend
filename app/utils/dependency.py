155a4241da677bb7f8bd2c623573dd56b9dc1338
amandeep-boot <aman509karan@gmail.com>
2025-06-16 21:12:57 +0530

    .example.env

--- app/utils/dependency.py
+++ app/utils/dependency.py
@@ -8,7 +8,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl= "/auth/login")
 def get_current_user(token :str = Depends(oauth2_scheme)):
     payload = decode_access_token(token)
     if payload is None :
-        raise HTTPException(\
+        raise HTTPException(
             status_code= status.HTTP_401_UNAUTHORIZED,
             detail = "Invalid or expired token ",
             headers= {"WWW-Authenticate": "Bearer"},    
