0b1d3d8337e3f79d02c6322107275583230321ac
Amandeep <aman509karan@gmail.com>
2026-04-14 01:47:35 +0530

    google login backned verification

--- app/database/models.py
+++ app/database/models.py
@@ -5,7 +5,11 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer , primary_key = True , index =True)
     email = Column(String , unique = True , index = True )
-    hashed_password = Column(String) 
+    hashed_password = Column(String, nullable=True) 
+    provider = Column(String, nullable=True)
+    google_sub = Column(String, unique=True, index=True)
+    name = Column(String, nullable=True)
+    avatar_url = Column(String, nullable=True)
 
 class Message(Base):
     __tablename__ = "chat_history" 
